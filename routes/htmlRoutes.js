4aff8a63db90cd5232839234e61a4d872d86530a
Haylee McLemore <hmmclemore@gmail.com>
2020-05-19 19:16:57 -0500

    updating html routes and handlebars to display forms

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,26 +1,39 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
+  // Load Landing page
   app.get("/", function(req, res) {
-    db.User.findAll({}).then(function(dbExamples) {
+    db.User.findAll({}).then(function() {
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+        msg: "Let's get Sweati.",
       });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
+  // Load Login page
+  app.get("/login", function(req, res) {
+    db.User.findAll({}).then(function() {
+      res.render("login");
     });
   });
+
+  // Load Register page
+  app.get("/register", function(req, res) {
+    db.User.findAll({}).then(function() {
+      res.render("register");
+    });
+  });
+
+  // // Load the login page
+  // app.get("/login", function(req, res) {
+  //   db.User.findOne({ where: { id: req.params.id } }).then(function(
+  //     dbExample
+  //   ) {
+  //     res.render("login", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
