dc1e2dc6accaf557c5cb7c8e38598206c9bf47fe
Haylee McLemore <hmmclemore@gmail.com>
2020-05-22 16:34:06 -0500

    routes progress

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,18 +6,18 @@ module.exports = function(app) {
     db.User.findAll({
       include: [
         {
-          model: db.Cardio
+          model: db.Cardio,
         },
         {
-          model: db.Mind
+          model: db.Mind,
         },
         {
-          model: db.Friends
+          model: db.Friends,
         },
         {
-          model: db.Strength
-        }
-      ]
+          model: db.Strength,
+        },
+      ],
     }).then(function(data) {
       res.json(data);
     });
@@ -29,18 +29,18 @@ module.exports = function(app) {
       where: { id: req.params.id },
       include: [
         {
-          model: db.Cardio
+          model: db.Cardio,
         },
         {
-          model: db.Mind
+          model: db.Mind,
         },
         {
-          model: db.Friends
+          model: db.Friends,
         },
         {
-          model: db.Strength
-        }
-      ]
+          model: db.Strength,
+        },
+      ],
     }).then(function(dbUser) {
       res.json(dbUser); //When we switch over to handlebars, we need to change this line to res.render("dash", {dbUser:dbUser})
     });
@@ -52,7 +52,7 @@ module.exports = function(app) {
       friends: req.body.friend,
       image: req.body.image,
       // change from body to user - body is just for testing purposes in postman until front end is ready
-      UserId: req.body.userId
+      UserId: req.body.userId,
     }).then(function(newFriend) {
       res.json(newFriend);
     });
@@ -65,5 +65,65 @@ module.exports = function(app) {
     ) {
       res.json(dbExample);
     });
+  });
+
+  app.post("/api/workout", function(req, res) {
+    console.log("we shoudls ave the body to the database", req.body);
+
+    if (req.body.workout_type === "cardio") {
+      db.Cardio.create({
+        // friend is pulling from front end - be sure to add "friend" as the variable
+        cardio_act: req.body.cardio_act,
+        start: req.body.start,
+        end: req.body.start.end,
+        // change from body to user - body is just for testing purposes in postman until front end is ready
+        UserId: req.body.userId
+      }).then(function(newWorkout) {
+        res.json(newWorkout);
+      });
+    } else if (req.body.workout_type === "mind") {
+      db.Mind.create({
+        // friend is pulling from front end - be sure to add "friend" as the variable
+        mind_act: req.body.mind_act,
+        start: req.body.start,
+        end: req.body.start.end,
+        // change from body to user - body is just for testing purposes in postman until front end is ready
+        UserId: req.body.userId
+      }).then(function(newWorkout) {
+        res.json(newWorkout);
+      });
+    } else if (req.body.workout_type === "strength") {
+      db.Strength.create({
+        // friend is pulling from front end - be sure to add "friend" as the variable
+        strength_act: req.body.strength_act,
+        start: req.body.start,
+        end: req.body.start.end,
+        // change from body to user - body is just for testing purposes in postman until front end is ready
+        UserId: req.body.userId,
+      }).then(function(newWorkout) {
+        res.json(newWorkout);
+      });
+    }
+  });
+
+  // Delete an workout by id
+  app.delete("/api/workout/:id", function(req, res) {
+    if (req.body.workout_type === "cardio") {
+      db.Cardio.destroy({ where: { id: req.params.id } }).then(function(data) {
+        res.json(data);
+      });
+    } else if (req.body.workout_type === "mind") {
+      db.Mind.destroy({ where: { id: req.params.id } }).then(function(
+        dbExample
+      ) {
+        res.json(dbExample);
+      });
+    } else if (req.body.workout_type === "strength") {
+      db.Strength.destroy({ where: { id: req.params.id } }).then(function(
+        dbExample
+      ) {
+        res.json(dbExample);
+      });
+    }
   });
 };
