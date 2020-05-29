a44fb49ecc362e985ba6858352a39e2f47975936
Haylee McLemore <hmmclemore@gmail.com>
2020-05-29 13:56:41 -0500

    updating styles for login page

--- models/index.js
+++ models/index.js
@@ -20,17 +20,17 @@ if (config.use_env_variable) {
   );
 }
 // Connection to Heroku
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Rx4S5d94&',
-    database: 'sweati_db'
-  });
-};
-connection.connect();
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Bob@@1#er',
+//     database: 'sweati_db'
+//   });
+// };
+// connection.connect();
 
 fs.readdirSync(__dirname)
   .filter(function(file) {
@@ -55,4 +55,4 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
-module.exports = connection;
+// module.exports = connection;
