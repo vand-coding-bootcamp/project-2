cc7bda1f19583730c4d357c9548dfbf197ea4356
John Pendergrass <john.pendergrass@me.com>
2020-06-01 14:12:37 -0500

    added psuedocode headers for each section, deleted examples code and handlebar

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,7 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all users of app (working)
-  app.get("/api/examples", function(req, res) {
-    db.User.findAll({
-      include: [
-        {
-          model: db.Cardio
-        },
-        {
-          model: db.Mind
-        },
-        {
-          model: db.Friends
-        },
-        {
-          model: db.Strength
-        }
-      ]
-    }).then(function(data) {
-      res.json(data);
-    });
-  });
+ 
 // =========================================================================================
 // ----------------------------------DASHBOARD APIS-----------------------------------------
 // =========================================================================================
@@ -94,14 +74,6 @@ module.exports = function(app) {
     });
   });
 
-  // Delete an example by id
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(
-  //     dbExample
-  //   ) {
-  //     res.json(dbExample);
-  //   });
-  // });
 
 // =========================================================================================
 // --------------------------------WORKOUT APIS---------------------------------------------
